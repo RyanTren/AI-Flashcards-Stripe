065808773a4df133a484cca68e1d855c505cdfd1
ryan <concepting@protonmail.com>
2024-08-13 01:18:05 -0400

    make appbar wider on login and sign up page

--- flashcard-saas/app/sign-in/[[...sign-in]]/page.js
+++ flashcard-saas/app/sign-in/[[...sign-in]]/page.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 
 export default function SignInPage(){
     return(
-    <Container maxWidth="sm">
+    <Container maxWidth="100vw">
         <AppBar position="static" sx={{backgroundColor: '#3f51b5'}}>
             <Toolbar>
                 <Typography variant="h6" sx={{flexGrow: 1}}>
