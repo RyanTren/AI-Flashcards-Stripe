9457d9aef388fb5ef6d0520515da006231a6fe41
ryan <concepting@protonmail.com>
2024-08-12 22:38:09 -0400

    Update page.js

--- flashcard-saas/app/page.js
+++ flashcard-saas/app/page.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import { Box, AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
 import Head from "next/head";
 
 export default function Home() {
@@ -14,16 +14,35 @@ export default function Home() {
 
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6">Flashcard Saas</Typography>
+          <Typography variant="h6" style={{flexGrow: 1}}>Flashcard Saas</Typography>
           <SignedOut>
-            <Button> Login</Button>
-            <Button> Sign Up</Button>
+            <Button color="inherit"> Login</Button>
+            <Button color="inherit"> Sign Up</Button>
           </SignedOut>
           <SignedIn>
             <UserButton />
           </SignedIn>
         </Toolbar>
       </AppBar>
+
+      <Box 
+        sx={{
+          textAlign: "center",
+          my: 4,
+        }}
+      >
+        <Typography variant="h2">Welcome to Flasher.io</Typography>
+
+        <Typography variant="h5">
+          {' '}
+          The easiest way to make flash cards from your custom input
+        </Typography>
+
+        <Button variant="contained" color = "primary" sx = {{mt: 2}}>
+          Get Started
+        </Button>
+
+      </Box>
     </Container>
   );
 }
